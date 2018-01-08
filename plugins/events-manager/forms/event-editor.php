a199fda834650d298f23af7ddbfe52cf2d5f89ac
sezj <sarahzachrich@gmail.com>
2018-01-08 12:22:52 -0500

    update event submission form and bookings list templates from plugin update

--- plugins/events-manager/forms/event-editor.php
+++ plugins/events-manager/forms/event-editor.php
@@ -1,7 +1,7 @@
 <?php
 /* WARNING! This file may change in the near future as we intend to add features to the event editor. If at all possible, try making customizations using CSS, jQuery, or using our hooks and filters. - 2012-02-14 */
 /* 
- * To ensure compatability, it is recommended you maintain class, id and form name attributes, unless you know what you're doing. 
+ * To ensure compatability, it is recommended you maintain class, id and form name attributes, unless you now what you're doing. 
  * You also must keep the _wpnonce hidden field in this form too.
  */
 global $EM_Event, $EM_Notices, $bp;
@@ -15,7 +15,7 @@ if( is_object($EM_Event) && !$EM_Event->can_manage('edit_events','edit_others_ev
 }elseif( !is_object($EM_Event) ){
 	$EM_Event = new EM_Event();
 }
-$required = apply_filters('em_required_html','<em>*</em>');
+$required = apply_filters('em_required_html','<i>*</i>');
 
 echo $EM_Notices;
 //Success notice
@@ -23,7 +23,7 @@ if( !empty($_REQUEST['success']) ){
 	if(!get_option('dbem_events_form_reshow')) return false;
 }
 ?>	
-<form enctype='multipart/form-data' id="event-form" method="post" action="<?php echo esc_url(add_query_arg(array('success'=>null))); ?>">
+<form enctype='multipart/form-data' id="event-form" class="em-event-admin-editor <?php if( $EM_Event->is_recurring() ) echo 'em-event-admin-recurring' ?>" method="post" action="<?php echo esc_url(add_query_arg(array('success'=>null))); ?>">
 	<div class="wrap">
 		<?php do_action('em_front_event_form_header'); ?>
 		<?php if(get_option('dbem_events_anonymous_submissions') && !is_user_logged_in()): ?>
@@ -38,14 +38,15 @@ if( !empty($_REQUEST['success']) ){
 					<input type="text" name="event_owner_email" id="event-owner-email" value="<?php echo esc_attr($EM_Event->event_owner_email); ?>" />
 				</p>
 				<?php do_action('em_front_event_form_guest'); ?>
+				<?php do_action('em_font_event_form_guest'); //deprecated ?>
 			</div>
 		<?php endif; ?>
-		<h3 class="event-form-name"><?php esc_html_e( 'Event Name', 'events-manager'); ?><?php echo $required; ?></h3>
-		<div class="inside event-form-name">					
-			<input type="text" name="event_name" id="event-name" value="<?php echo esc_attr($EM_Event->event_name,ENT_QUOTES); ?>" />
-			<p><em><?php esc_html_e('Example: Information Session', 'events-manager'); ?></em></p>
+		<h3 class="event-form-name"><?php esc_html_e( 'Event Name', 'events-manager'); ?></h3>
+		<div class="inside event-form-name">
+			<input type="text" name="event_name" id="event-name" value="<?php echo esc_attr($EM_Event->event_name,ENT_QUOTES); ?>" /><?php echo $required; ?>
+			<br />
+			<?php esc_html_e('Example: Information session', 'events-manager'); ?>
 			<?php em_locate_template('forms/event/group.php',true); ?>
-
 		</div>
 					
 		<h3 class="event-form-when"><?php esc_html_e( 'Date and Time', 'events-manager'); ?></h3>
@@ -53,9 +54,9 @@ if( !empty($_REQUEST['success']) ){
 		<?php 
 			if( empty($EM_Event->event_id) && $EM_Event->can_manage('edit_recurring_events','edit_others_recurring_events') && get_option('dbem_recurrence_enabled') ){
 				em_locate_template('forms/event/when-with-recurring.php',true);
-			} elseif( $EM_Event->is_recurring()  ){
+			}elseif( $EM_Event->is_recurring()  ){
 				em_locate_template('forms/event/recurring-when.php',true);
-			} else{
+			}else{
 				em_locate_template('forms/event/when.php',true);
 			}
 		?>
@@ -74,8 +75,9 @@ if( !empty($_REQUEST['success']) ){
 				<?php if( get_option('dbem_events_form_editor') && function_exists('wp_editor') ): ?>
 					<?php wp_editor($EM_Event->post_content, 'em-editor-content', array('textarea_name'=>'content') ); ?> 
 				<?php else: ?>
-					<textarea name="content" rows="10"><?php echo $EM_Event->post_content ?></textarea>
-					<p><em><?php esc_html_e( 'Details about the event.', 'events-manager')?> <?php esc_html_e( 'HTML allowed.', 'events-manager')?></em></p>
+					<textarea name="content" rows="10" style="width:100%"><?php echo $EM_Event->post_content ?></textarea>
+					<br />
+					<?php esc_html_e( 'Details about the event.', 'events-manager')?> <?php esc_html_e( 'HTML allowed.', 'events-manager')?>
 				<?php endif; ?>
 			</div>
 			<div class="event-extra-details">
@@ -104,12 +106,11 @@ if( !empty($_REQUEST['success']) ){
 	</div>
 	<p class="submit">
 	    <?php if( empty($EM_Event->event_id) ): ?>
-	    <input type='submit' class='button-primary' value='<?php echo esc_attr(sprintf( __('Submit %s','events-manager'), __('Event','events-manager') )); ?>' />
+	    <input type='submit' class='button' value='<?php echo esc_attr(sprintf( __('Submit %s','events-manager'), __('Event','events-manager') )); ?>' />
 	    <?php else: ?>
-	    <input type='submit' class='button-primary' value='<?php echo esc_attr(sprintf( __('Update %s','events-manager'), __('Event','events-manager') )); ?>' />
+	    <input type='submit' class='button' value='<?php echo esc_attr(sprintf( __('Update %s','events-manager'), __('Event','events-manager') )); ?>' />
 	    <?php endif; ?>
 	</p>
-	
 	<input type="hidden" name="event_id" value="<?php echo $EM_Event->event_id; ?>" />
 	<input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce('wpnonce_event_save'); ?>" />
 	<input type="hidden" name="action" value="event_save" />
