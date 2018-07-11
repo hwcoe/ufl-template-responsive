20f7a21fe5f5c346de95065b4ba22503a0f43e72
sezj <sarahzachrich@gmail.com>
2018-07-11 14:21:40 -0400

    Create page template to list site posts/pages with various custom meta

--- template-options-output.php
+++ template-options-output.php
@@ -7,181 +7,109 @@ get_header(); ?>
 
 		<div class="container">
 			<div id="content" role="main">
-        
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	  
 			<h2 class="entry-title">Options Check Theme</h2>
-            
-            <div class="entry-content">
-            
-            <p>Use of_of_get_option($id,$default) to return option values.</p>
-            
-            <h3>Basic Options</h3>
-            
-            <dl>
-            <dt>type: text (mini)</dt>
-            <dd>of_get_option('example_text_mini'): <?php echo of_get_option('example_text_mini', 'no entry'); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: text</dt>
-            <dd>of_get_option('example_text'): <?php echo of_get_option('example_text', 'no entry'); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: textarea</dt>
-            <dd>of_get_option('example_textarea'): <?php echo of_get_option('example_textarea', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: select (mini)</dt>
-            <dd>of_get_option('example_select'): <?php echo of_get_option('example_select', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: select2 (wide)</dt>
-            <dd>of_get_option('example_select_wide'): <?php echo of_get_option('example_select_wide', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: select</dt>
-            <dd>of_get_option('example_select_categories'): category id = <?php echo of_get_option('example_select_categories', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: select</dt>
-            <dd>of_get_option('example_select_pages'): page id = <?php echo of_get_option('example_select_pages', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: radio</dt>
-            <dd>of_get_option('example_radio'): <?php echo of_get_option('example_radio', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: checkbox</dt>
-            <dd>of_get_option('example_checkbox'): <?php echo of_get_option('example_checkbox', 'no entry' ); ?></dd>
-            </dl>
-            
-            <hr/>
-            
-            <h3>Advanced Options</h3>
-            
-            <dl>
-            <dt>type: uploader</dt>
-            <dd>of_get_option('example_uploader'): <?php echo of_get_option('example_uploader', 'no entry'); ?></dd>
-            <?php if ( of_get_option('example_uploader') ) { ?>
-            <img src="<?php echo of_get_option('example_uploader'); ?>" />
-			<?php } ?>
-            </dl>
-            
-            <dl>
-            <dt>type: image</dt>
-            <dd>of_get_option('images'): <?php echo of_get_option('example_images', 'no entry' ); ?></dd>
-            </dl>
-            
-            <dl>
-            <dt>type: multicheck</dt>
-            <dd>of_get_option('multicheck'):
-            <?php $multicheck = of_get_option('example_multicheck', 'none' ); ?>
-			<?php print_r($multicheck); ?>
-            </dd>
-            </dl>
-            
-            <p>The array sent in the options panel was defined as:<br>
-            <?php
-			$test_array_jr = array("one" => "French Toast","two" => "Pancake","three" => "Omelette","four" => "Crepe","five" => "Waffle"); 
-			print_r($test_array_jr);
-			?>
-            </p>
-            
-            <p>You can get the value of all items in the checkbox array:</p>
-            <ul>
-            <?php
-			if ( is_array($multicheck) ) {
-				foreach ($multicheck as $key => $value) {
-					// If you need the option's name rather than the key you can get that
-					$name = $test_array_jr[$key];
-					// Prints out each of the values
-					echo '<li>' . $key . ' (' . $name . ') = ' . $value . '</li>';
-				}
-			}
-			else {
-				echo '<li>There are no saved values yet.</li>';
-			}
-			?>
-            </ul>
-            
-            <p>You can also get an individual checkbox value if you know what you are looking for.  In this example, I'll check for the key "one", which is an item I sent in the array for checkboxes:</p>
-            
-            <p>The value of the multicheck box "one" of example_multicheck is: 
-            <b>
-            <?php
-            if (isset($multicheck['one']) ) {
-				echo $multicheck['one'];
-			} else {
-				echo 'no entry';
-			}
-			?>
-            </b>
-            </p>
-            
-            
-            <dl>
-            <dt>type: background</dt>
-            <dd>of_get_option('background'):
-            <?php $background = of_get_option('example_background');
-            if ($background) {
-				if ($background['image']) {
-					echo '<span style="display: block; height: 200px; width: 200px; background:url('.$background['image']. ') "></span>';
-					echo '<ul>';
-					foreach ($background as $i=>$param){
-						echo '<li>'.$i . ' = ' . $param.'</li>';
-				}
-				echo '</ul>';
-				} else {
-					echo '<span style="display: inline-block; height: 20px; width: 20px; background:'.$background['color']. ' "></span>';
-					echo '<ul>';
-					echo '<li>'.$background['color'].'</li>';
-					echo '</ul>';
-				}	
-            } else {
-            	echo "no entry";
-            }; ?>
-            </span>
-            </dd>
-            </dl>
-            
-            <dl>
-            <dt>type: colorpicker</dt>
-            <dd>of_get_option('colorpicker'):
-            <span style="color:<?php echo of_get_option('example_colorpicker', '#000' ); ?>">
-			<?php echo of_get_option('example_colorpicker', 'no entry' ); ?>
-            </span>
-            </dd>
-            </dl>
-            
-            <dl>
-            <dt>type: typography</dt>
-            <dd>of_get_option('typography'):
-            <?php $typography = of_get_option('example_typography');
-            if ($typography) {
-				echo '<span style="font:'.$typography['size'] . ' ' . $typography['face']. ' ' . $typography['style'] . '; color:'.$typography['color'].';">Some sample text in your style</span>';
-				
-				echo '<ul>';
-				foreach ($typography as $i=>$param) {
-					echo '<li>'.$i . ' = ' . $param.'</li>';
-				}
-					echo '</ul>';
-			} else {
-				echo "no entry";
-			} ?>
-            </dd>
-            </dl>
-            
-            </div>
-            </div>
+		
+				<div class="entry-content">
+				<?php
+					echo "<h3>Non Default Page template</h3>";
+					echo "<ul>";
+					$args = [
+						'post_type' => 'page',
+						'posts_per_page' => -1,
+						'meta_query' => array(
+							array(
+							'key'     => '_wp_page_template',
+							'value'   => array('template-blank.php', 'template-full-width.php', 'template-no-container.php', 'template-options-output.php','membersonly.php')
+							)
+						)
+					];
+					$pages = get_posts( $args );
+					foreach ( $pages as $page ){
+						echo '<li><a href="' . get_permalink( $page ) . '">'.get_the_title( $page ).'</a></li>';
+					}
+					echo "</ul>";
+				?>
+				<?php
+					echo "<h3>Members only</h3>";
+					echo "<ul>";
+					$args = [
+						'post_type' => 'page',
+						'posts_per_page' => -1,
+						'meta_query' => array(
+							array(
+							'key'     => 'custom_meta_visitor_auth_level',
+							'value'   => array('GatorLink Users', 'WordPress Users')
+							)
+						)
+					];
+					$pages = get_posts( $args );
+					foreach ( $pages as $page ) {
+						echo '<a href="' . get_permalink( $page ) . '">'.get_the_title( $page ).'</a><br />';
+					}
+					echo "</ul>";
+
+				?>
+
+				<?php
+					echo "<h3>Post/Page with Subtitle</h3>";
+					echo "<ul>";
+					$args = [
+						// 'post_type' => 'page',
+						'post_type' => array( 'post', 'page'),
+						'posts_per_page' => -1,		
+						'meta_query' => array(
+							'relation' => 'OR',
+							array(
+							'key'     => 'custom_meta_page_subtitle',
+							'value'   => '',
+							'compare' => '!='
+							),
+							array(
+							'key'     => 'custom_meta_post_subtitle',
+							'value'   => '',
+							'compare' => '!='
+							),
+						),
+					];
+					$pages = get_posts( $args );
+					foreach ( $pages as $page ) {
+						echo '<a href="' . get_permalink( $page ) . '">'.get_the_title( $page ).'</a><br />';
+					}
+					echo "</ul>";
+				?>
+				<?php
+					echo "<h3>Post/Page with Title Override</h3>";
+					echo "<ul>";
+					$args = [
+						// 'post_type' => 'page',
+						'post_type' => array( 'post', 'page'),
+						'posts_per_page' => -1,		
+						'meta_query' => array(
+							'relation' => 'OR',
+							array(
+							'key'     => 'custom_meta_page_title_override',
+							'value'   => '',
+							'compare' => '!='
+							),
+							array(
+							'key'     => 'custom_meta_post_title_override',
+							'value'   => '',
+							'compare' => '!='
+							),
+						),
+					];
+					$pages = get_posts( $args );
+					foreach ( $pages as $page ) {
+						echo '<a href="' . get_permalink( $page ) . '">'.get_the_title( $page ).'</a><br />';
+					}
+					echo "</ul>";
+
+				?>
+
+				</div><!-- #entry-content -->
 			
 			</div><!-- #content -->
 		</div><!-- #container -->
-        
+	  
 <?php get_footer(); ?>
