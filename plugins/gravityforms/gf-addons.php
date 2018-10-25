9bc1362ca6bb665beddf1e1551fd23fabe0be22c
sezj <sarahzachrich@gmail.com>
2018-10-25 11:54:20 -0400

    Allow multi-select options to be limited to 2 by applying .limit-options-to-2 class to form field

--- plugins/gravityforms/gf-addons.php
+++ plugins/gravityforms/gf-addons.php
@@ -1,8 +1,8 @@
 <?php
-/* ----------------------------------------------------------------------------------- */
-/* 	DO NOT COPY/PASTE BETWEEN TEST AND PRODUCTION SERVERS without checking form IDs
+/* -------------------------------------------------------------------------------------------- */
+/* 	DO NOT COPY/PASTE BETWEEN TEST AND PRODUCTION SERVERS without checking and editing form IDs
 /*	Form IDs are often different between sites and filters/actions won't work!
-/* ----------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------------------- */
 
 // Set options for multi select fields
 function set_chosen_options($form){
@@ -14,8 +14,9 @@ function set_chosen_options($form){
 		function set_chosen_options_js(options, element){
 			//form id = 25, field id = 39
 			// graduate student affairs site, University of Puerto Rico Mayaguez SHPE ENGINE Participation form
-			if (element.attr('id') == 'input_25_39'){
-				//limit number of options to 2
+			// if (element.attr('id') == 'input_48_39'){
+			if (element.attr('class') == 'limit_options_to_2'){
+				//limit number of options to 2 for "Graduate Program of Interest" field
 				options.max_selected_options = 2;   
 			}
 			
@@ -30,7 +31,7 @@ function set_chosen_options($form){
 
 add_action("gform_pre_render", "set_chosen_options");
 
-// Gravity Forms maintenance mode - uncomment to use
+// Gravity Forms maintenance mode - uncomment filter to use
 function maintenance_mode( $form_string, $form ) {
     $form_string = '<p>Forms have been temporarily disabled for scheduled maintenance. Please check back later.</p>';
 
